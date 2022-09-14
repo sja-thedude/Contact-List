9fe9cba2eb5b94c9f7a83bda75eb9002088f6986
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-09-15 01:41:37 +0300

    Add Contact Form add functionality

--- src/components/contacts/AddContact/AddContact.jsx
+++ src/components/contacts/AddContact/AddContact.jsx
@@ -1,8 +1,74 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "../../../App.css";
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import { ContactService } from '../../../services/ContactService';
+
 
 let AddContact = () => {
+
+    let navigate = useNavigate();
+
+    let [state , setState] = useState({
+        loading : false,
+        contact : {
+            name : '',
+            photo : '',
+            mobile : '',
+            email : '',
+            company : '',
+            title : '',
+            groupId : ''
+        },
+        groups : [],
+        errorMessage : ''
+        
+    });
+
+    let updateInput = (event) =>{
+        setState({
+            ...state,
+            contact: {
+                ...state.contact,
+                [event.target.name] : event.target.value
+            }
+        });
+    };
+
+    let addContacts = async () => {
+        
+        try {
+            setState({...state, loading: true});
+            let response = await ContactService.getGroups();
+            setState({
+                ...state,
+                loading: false,
+                groups: response.data
+            });
+        }
+        catch (error) {
+        }
+    }
+
+    useEffect(() => {
+        addContacts()
+},);
+
+let submitForm = async (event) => {
+    event.preventDefault();
+    try {
+        let response = await ContactService.createContact(state.contact);
+        if(response){
+            navigate('/contact/list', {replace: true});
+        }
+    }
+    catch (error) {
+        setState({...state , errorMessage: error.message});
+        navigate('/contact/add', {replace: false});
+    }
+};
+
+    let {loading , contact , groups , errorMessage} = state;
+
     return (
         <React.Fragment>
             <section className='add-contact p-3'>
@@ -15,28 +81,71 @@ let AddContact = () => {
                     </div>
                     <div className="row">
                         <div className="col-md-4">
-                            <form action="">
+                            <form onSubmit={submitForm}>
                                 <div className="mb-2">
-                                    <input type="text" className='form-control' placeholder="Name"/>
+                                    <input
+                                        required={true}
+                                        name="name"
+                                        value={contact.name}
+                                        onChange={updateInput}
+                                        type="text" className='form-control' placeholder="Name"/>
                                 </div>
                                 <div className="mb-2">
-                                    <input type="text" className='form-control' placeholder="Photo Url"/>
+                                    <input
+                                    required={true}
+                                    name="photo"
+                                    value={contact.photo}
+                                    onChange={updateInput}
+                                    type="text" className='form-control' placeholder="Photo Url"/>
                                 </div>
                                 <div className="mb-2">
-                                    <input type="number" className='form-control' placeholder="Mobile"/>
+                                    <input
+                                    required={true}
+                                    name="mobile"
+                                    value={contact.mobile}
+                                    onChange={updateInput}
+                                    type="number" className='form-control' placeholder="Mobile"/>
                                 </div>
                                 <div className="mb-2">
-                                    <input type="email" className='form-control' placeholder="Email"/>
+                                    <input
+                                    required={true}
+                                    name="email"
+                                    value={contact.email}
+                                    onChange={updateInput}
+                                    type="email" className='form-control' placeholder="Email"/>
                                 </div>
                                 <div className="mb-2">
-                                    <input type="text" className='form-control' placeholder="Company"/>
+                                    <input
+                                    required={true}
+                                    name="company"
+                                    value={contact.company}
+                                    onChange={updateInput}
+                                    type="text" className='form-control' placeholder="Company"/>
                                 </div>
                                 <div className="mb-2">
-                                    <input type="text" className='form-control' placeholder="Title"/>
+                                    <input
+                                    required={true}
+                                    name="title"
+                                    value={contact.title}
+                                    onChange={updateInput}
+                                    type="text" className='form-control' placeholder="Title"/>
                                 </div>
                                 <div className="mb-2">
-                                    <select className="form-control">
+                                    <select 
+                                    required={true}
+                                    name="groupId"
+                                    value={contact.groupId}
+                                    onChange={updateInput}
+                                    className="form-control">
                                         <option value="">Select a Group</option>
+                                        {
+                                            groups.length > 0 &&
+                                                groups.map(group => {
+                                                    return(
+                                                        <option key={group.id} value={group.id}>{group.name}</option>
+                                                    )
+                                                })
+                                        }
                                     </select>
                                 </div>
                                 <div className="mb-2">
--- src/components/contacts/ContactList/ContactList.jsx
+++ src/components/contacts/ContactList/ContactList.jsx
@@ -3,7 +3,6 @@ import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 import "../../../App.css";
 import { ContactService } from '../../../services/ContactService';
-//import Spinner from "../../spinner/Spinner";
 
 let ContactList = () => {
 
